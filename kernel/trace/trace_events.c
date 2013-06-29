94e8ac4105ca45192b9cffebace489d1df7c7b49
Tom Zanussi <tom.zanussi@linux.intel.com>
2013-06-29 00:08:04 -0500

    tracing: Simplify code for showing of soft disabled flag
    Rather than enumerating each permutation, build the enable state
    string up from the combination of states.  This also allows for the
    simpler addition of more states.
    
    Link: http://lkml.kernel.org/r/9aff5af6dee2f5a40ca30df41c39d5f33e998d7a.1372479499.git.tom.zanussi@linux.intel.com
    
    Signed-off-by: Tom Zanussi <tom.zanussi@linux.intel.com>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Jesse Chan <cjx123@outlook.com>

--- kernel/trace/trace_events.c
+++ kernel/trace/trace_events.c
@@ -682,7 +682,7 @@ event_enable_read(struct file *filp, char __user *ubuf, size_t cnt,
 {
 	struct ftrace_event_file *file;
 	unsigned long flags;
-	char *buf;
+	char buf[4] = "0";
 
 	mutex_lock(&event_mutex);
 	file = event_file_data(filp);
@@ -693,15 +693,15 @@ event_enable_read(struct file *filp, char __user *ubuf, size_t cnt,
 	if (!file)
 		return -ENODEV;
 
-	if (flags & FTRACE_EVENT_FL_ENABLED) {
-		if (flags & FTRACE_EVENT_FL_SOFT_DISABLED)
-			buf = "0*\n";
-		else if (flags & FTRACE_EVENT_FL_SOFT_MODE)
-			buf = "1*\n";
-		else
-			buf = "1\n";
-	} else
-		buf = "0\n";
+	if (file->flags & FTRACE_EVENT_FL_ENABLED &&
+	    !(file->flags & FTRACE_EVENT_FL_SOFT_DISABLED))
+		strcpy(buf, "1");
+
+	if (file->flags & FTRACE_EVENT_FL_SOFT_DISABLED ||
+	    file->flags & FTRACE_EVENT_FL_SOFT_MODE)
+		strcat(buf, "*");
+
+	strcat(buf, "\n");
 
 	return simple_read_from_buffer(ubuf, cnt, ppos, buf, strlen(buf));
 }
