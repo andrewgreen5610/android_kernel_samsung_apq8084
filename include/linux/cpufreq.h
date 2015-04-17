6b5890eab221daae6954d0aecf7b0df0a1f69d4d
Ruchi Kandoi <kandoiruchi@google.com>
2015-04-17 16:33:29 -0700

    sched: cpufreq: Adds a field cpu_power in the task_struct
    cpu_power has been added to keep track of amount of power each task is
    consuming. cpu_power is updated whenever stime and utime are updated for
    a task. power is computed by taking into account the frequency at which
    the current core was running and the current for cpu actively
    running at hat frequency.
    
    Bug: 21498425
    Change-Id: Ic535941e7b339aab5cae9081a34049daeb44b248
    Signed-off-by: Ruchi Kandoi <kandoiruchi@google.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -16,6 +16,7 @@
 #include <linux/kobject.h>
 #include <linux/notifier.h>
 #include <linux/sysfs.h>
+#include <asm/cputime.h>
 
 #ifdef CONFIG_MSM_LIMITER
 int cpufreq_set_gov(char *target_gov, unsigned int cpu);
@@ -533,5 +534,11 @@ static inline int cpufreq_generic_exit(struct cpufreq_policy *policy)
 	cpufreq_frequency_table_put_attr(policy->cpu);
 	return 0;
 }
+
+/*********************************************************************
+ *                         CPUFREQ STATS                             *
+ *********************************************************************/
+
+void acct_update_power(struct task_struct *p, cputime_t cputime);
 
 #endif /* _LINUX_CPUFREQ_H */
