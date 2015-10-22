d2496fb7d09148603e8f955db0b09f5d06355581
Tkkg1994 <luca.grifo@outlook.com>
2015-10-22 20:05:28 +0200

    cpufreq: implement cpufreq_quick_get_util()
    Just gives us the last load known to the current cpu
    governor with a much smaller overhead.
    
    Signed-off-by: Blechd0se <alex.christ@hotmail.de>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -150,6 +150,7 @@ void cpufreq_sysfs_remove_file(const struct attribute *attr);
 unsigned int cpufreq_get(unsigned int cpu);
 unsigned int cpufreq_quick_get(unsigned int cpu);
 unsigned int cpufreq_quick_get_max(unsigned int cpu);
+unsigned int cpufreq_quick_get_util(unsigned int cpu);
 void disable_cpufreq(void);
 
 u64 get_cpu_idle_time(unsigned int cpu, u64 *wall, int io_busy);
@@ -170,6 +171,11 @@ static inline unsigned int cpufreq_quick_get_max(unsigned int cpu)
 {
 	return 0;
 }
+static inline unsigned int cpufreq_quick_get_util(unsigned int cpu);
+{
+	return 0;
+}
+
 static inline void disable_cpufreq(void) { }
 #endif
 
